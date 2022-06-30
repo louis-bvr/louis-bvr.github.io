52717a4e16afbc58bb35aefcfd7c5512a515cc44
louis-bvr <106966891+louis-bvr@users.noreply.github.com>
2022-06-30 22:46:52 +0200

    Add files via upload

---
+++ data/TPE/TPE Site Version Présention/data/anime.js
@@ -0,0 +1,279 @@
+/*reset de base a false*/
+const sr = ScrollReveal({
+    duration: '2000',
+    visibility: 'hidden',
+    reset: 'true',
+});
+
+sr.reveal ('#intro_1', {    
+    origin: 'top',
+    distance: '100px',
+});
+
+sr.reveal ('#intro_2', {
+    delay: '1250',
+});
+
+sr.reveal ('#intro_3', {
+    delay: '1750',
+});
+
+sr.reveal ('#intro_4', {
+    delay: '2250',
+});
+
+sr.reveal ('#plana', {
+    delay: '3000',
+});
+
+sr.reveal ('#plana1', {
+    delay: '3250',
+});
+
+sr.reveal ('#plana2', {
+    delay: '3500',
+});
+
+sr.reveal ('#planb', {
+    delay: '4000',
+});
+
+sr.reveal ('#planb1', {
+    delay: '4250',
+});
+
+sr.reveal ('#planb2', {
+    delay: '4500',
+});
+
+/* I.A */
+sr.reveal ('#a1_2', {
+    delay: '500',
+});
+
+sr.reveal ('#Iaimg1', {
+    delay: '750',
+});
+
+sr.reveal ('#a1_3', {
+    delay: '1000'
+});
+
+sr.reveal ('#a1_4', {
+    delay: '1500',
+});
+
+sr.reveal ('#a1_5', {
+    delay: '2000',
+});
+
+sr.reveal ('#a1_6', {
+    delay: '2500',
+});
+
+sr.reveal ('#Iaimg2', {
+    delay: '500',
+});
+
+sr.reveal ('#li1a1', {
+    delay: '750',
+});
+
+sr.reveal ('#li2a1', {
+    delay: '1000',
+});
+
+sr.reveal ('#a1_7', {
+    delay: '1500',
+});
+
+sr.reveal ('#Iaimg3', {
+    delay: '1750',
+});
+
+sr.reveal ('#a1_8', {
+    delay: '2500',
+});
+
+/*I.B*/
+
+sr.reveal ('#b1_2', {
+    delay: '500',
+});
+
+sr.reveal ('#b1_2_1', {
+    delay: '1000',
+});
+
+sr.reveal ('#b1_3', {
+    delay: '1500',
+});
+
+sr.reveal ('#gif4_1', {
+    delay: '2000',
+});
+
+sr.reveal ('#li1b1', {
+    delay: '2000',
+});
+
+sr.reveal ('#li2b1', {
+    delay: '2250',
+});
+
+sr.reveal ('#li3b1', {
+    delay: '2500',
+});
+
+sr.reveal ('#b1_3_1', {
+    delay: '3000',
+});
+
+sr.reveal ('#gif4_2', {
+    delay: '3500',
+});
+
+sr.reveal ('#b1_4', {
+    delay: '4000',
+});
+
+sr.reveal ('#b1_5', {
+    delay: '4500',
+});
+
+
+sr.reveal ('#b1_6', {
+    delay: '500',
+});
+
+sr.reveal ('#Ibimg1', {
+    delay: '1000',
+});
+
+sr.reveal ('#gif2', {
+    delay: '1250',
+});
+
+/* IIa */
+
+sr.reveal ('#a2_1', {
+    delay: '500'
+});
+
+sr.reveal ('#a2li1', {
+    delay: '1000'
+});
+
+sr.reveal ('#a2li2', {
+    delay: '1250'
+});
+
+sr.reveal ('#a2li3', {
+    delay: '1500'
+});
+
+sr.reveal ('#a2li4', {
+    delay: '1750'
+});
+
+sr.reveal ('#a2_1bis', {
+    delay: '2000'
+});
+
+sr.reveal ('#a2_2', {
+    delay: '2500'
+});
+
+sr.reveal ('#a2_3', {
+    delay: '500'
+});
+
+sr.reveal ('#bac', {
+    delay: '1000'
+});
+
+sr.reveal ('#a2_4', {
+    delay: '500'
+});
+
+sr.reveal ('#foncserv', {
+    delay: '1000'
+});
+
+sr.reveal ('#fp', {
+    delay: '1500'
+});
+
+sr.reveal ('#fp1', {
+    delay: '2000'
+});
+
+sr.reveal ('#fp2', {
+    delay: '2500'
+});
+
+sr.reveal ('#fp3', {
+    delay: '2500'
+});
+
+sr.reveal ('#fs', {
+    delay: '3000'
+});
+
+sr.reveal ('#fs1', {
+    delay: '3500'
+});
+
+sr.reveal ('#fs2', {
+    delay: '3750'
+});
+
+sr.reveal ('#fs3', {
+    delay: '4000'
+});
+
+sr.reveal ('#fs4', {
+    delay: '4250'
+});
+
+sr.reveal ('#a2_6', {
+    delay: '4750'
+});
+
+sr.reveal ('#b2_1', {
+    delay: '500'
+});
+
+sr.reveal ('#b2_2', {
+    delay: '1000'
+});
+
+sr.reveal ('#b2_3', {
+    delay: '1500'
+});
+
+sr.reveal ('#b2_4', {
+    delay: '2000'
+});
+
+sr.reveal ('#b2_5', {
+    delay: '2500'
+});
+
+sr.reveal ('#IIbimg1',{
+    delay: '3000'
+});
+
+sr.reveal ('#b2_6', {
+    delay: '3500'
+});
+
+/*
+sr.reveal ('#b2_', {
+    delay: ''
+});
+*/
+
+sr.reveal ('#ccl_1', {
+    delay: '500'
+});
